bd17cc1f11538d6a5e405d6b6b753eea09607b88
dilaraceltek <dilara.celtek@etiya.com>
2023-04-05 16:14:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+print("Merhaba Etiya") 
+print("Hoş geldiniz") 
+
+#yorum
+
+#değişkenler-start
+#metinsel, numerik, objeler çift tırnak içinde yazılır.
+#değişkenler tanımlandıktan sonra kullanılır.
+#metinsel ifadeler string ile tutulur.
+text = "Merhaba Etiya, hoş geldiniz"
+
+print(text)
+print(text)
+text = "Selam"
+print(text)
+print(text)
+print(text)
+
+studentCount = 45 #int, integer= tam sayı
+print(studentCount + 5)
+
+averagePoint = 25.5 #double, decimal, float = ondalıklı sayı
+print(averagePoint)
+
+
+isVerified = True #bool, boolean = True, False verinin doğruluğunu gösterirken kullanılır.
+print(isVerified)
+
+#type verinin tipini gösterir.
+print(type(text))
+print(type(studentCount))
+print(type(isVerified))
+
+#değişkenler-end
+
+#operatörler - start
+number = 10
+#matematiksel operatörler- mantıksal operatörler
+print(number + 10)
+print(number - 5)
+print(number / 2) #float sonucu verir.
+print(number * 2)
+
+#mod operatörü = kalan sayıyı verir.
+print(number %2)
+
+#operatörler-end
+
+#mantıksal operatörler- karşılaştırma operatörleri = < > => ==
+print(number == 10)
+print(number == 11)
+print(number != 10) #eşit olmama durumu
