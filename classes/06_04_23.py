e9e027ae34e814f9337877209b34503de4a0cece
dilaraceltek <dilara.celtek@etiya.com>
2023-04-06 13:31:51 +0300

    2nd lesson

---
+++ classes/06_04_23.py
@@ -0,0 +1,26 @@
+#diziler-listeler
+
+sayilar = [100,200,300,400,500, "merhaba"]   #listedeki veri tipinin uyumlu olmasına gerek yoktur.
+#programcı 0'dan saymaya başlar. yani ilk bşlangıç değeri 0. olarak geçer. index = 0
+print(sayilar[1])
+
+print(sayilar)
+sayilar.append(100)
+sayilar.append(600)        #append, listenin sonuna eleman ekler.
+
+print(sayilar)
+
+sayilar.pop(0)             #pop, index-parametre verilmediği durumda son indexi siler.
+print(sayilar)             #indexe göre çalışır.
+
+sayilar.remove(100)        #listedeki değere göre çalışır. birden fazla aynı değere sahip veri varsa ilk değer silinir.
+print(sayilar)
+
+sayilar.extend([700,800,900])           #extend, iki listeyi birleştirir.
+print(sayilar)                         #apend aksine tek bir değer eklemez.
+sayilarEkleme= [10,40,90]
+sayilar.extend(sayilarEkleme)
+print(sayilar)
+
+sayilar.clear()              #dizinin içini boşaltır, siler.
+print(sayilar)
