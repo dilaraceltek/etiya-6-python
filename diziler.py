9faceca74e91b15dcd459a6cc2cf86b2befb7252
dilaraceltek <dilara.celtek@etiya.com>
2023-04-06 08:22:55 +0300

    diziler

---
+++ diziler.py
@@ -0,0 +1,15 @@
+#Liste  [] olan yerde liste var demektir.
+krediler = ["Hızlı Kredi", "Maaşını Halkbank'tan Alanlara Özel", "Mutlu Emekli İhtiyaç Kredisi"]  
+
+print(krediler)
+print(krediler[0])     #python'da diziler 0'dan başlar. index sayısı
+print(krediler[1])
+print(krediler[2])
+print(len(krediler))   #length liste içinde sayıyı verir.
+
+krediler[0] = "Çabuk Kredi"
+print(krediler)
+
+print(krediler[5])    #liste 3 elemanlı olduğu için hata verecektir.
+print(krediler[0])
+
