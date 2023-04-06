c9e7c2891f8287da3c3241a191e7843c8bcc6895
dilaraceltek <dilara.celtek@etiya.com>
2023-04-06 08:13:02 +0300

    degisken

---
+++ değişkenler.py
@@ -0,0 +1,22 @@
+# 1st Day Homework
+#Programlamanın amacı, elimizdeki veriyi başka bir görüntü ortamında sunmayı sağlamak.
+
+baslik = "HABERİNİZ OLSUN" #string
+vade = 12                  #int
+faizOrani1 = 1.47          #ondalıklı sayıları nokta ile tut - float // 
+faizOrani2 = 1.44          #değişken tanımlarken boşluk bırakmamaya, sayı ile başlamamaya ve aralarda özel karakter kullanmamaya dikkat et.
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+
+mesaj= "Hoşgeldin"         #verileri değişkenlerde tutmanın en önemli nedeni, değişiklik yapmanın daha kolay olmasıdır.
+musteriAdi = "Dilara"
+musteriSoyadi = "Çeltek"
+sonucMesaj= mesaj+" "+musteriAdi+" "+musteriSoyadi  
+print(sonucMesaj) 
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1+sayi2)
---
+++ sart.py
@@ -0,0 +1,20 @@
+#Şart (If)
+
+dolarDun = 7.65
+dolarBugun = 7.65
+
+if dolarDun>dolarBugun:          #if bloğunun altından yazıldığından emin olunmalı
+    print("Azalış oku")          
+    print("Bitti")
+elif dolarDun<dolarBugun:
+    print("Artış oku")
+else:
+    print("Eşittir oku")
+print("Bitti")
+    
+
+#if dolarDun<dolarBugun:
+    #print("Artış oku")
+
+#if dolarDun==dolarBugun:
+    #print("Eşittir oku")
