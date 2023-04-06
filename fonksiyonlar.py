dac090d774497439e95b882597061ed3df7decf2
dilaraceltek <dilara.celtek@etiya.com>
2023-04-06 08:43:05 +0300

    1st day homework

---
+++ fonksiyonlar.py
@@ -0,0 +1,28 @@
+print("İlk sayfa")
+krediler = ["Hızlı Kredi", "Maaşını Halkbank'tan Alanlara Özel", "Mutlu Emekli İhtiyaç Kredisi"] 
+
+for kredi in krediler:      #alias 
+    print(kredi)
+
+print("İkinci sayfa")
+krediler = ["Hızlı Kredi", "Maaşını Halkbank'tan Alanlara Özel", "Mutlu Emekli İhtiyaç Kredisi"] 
+
+for kredi in krediler:      #alias 
+    print(kredi)
+
+print("Üçüncü sayfa")
+krediler = ["Hızlı Kredi", "Maaşını Halkbank'tan Alanlara Özel", "Mutlu Emekli İhtiyaç Kredisi"] 
+
+for kredi in krediler:      #alias 
+    print(kredi)
+
+
+#Yukarıdaki yazım şekli yerinde fonksiyon kullanıyoruz.
+
+def kredileriListele():
+    krediler = ["Hızlı Kredi", "Maaşını Halkbank'tan Alanlara Özel", "Mutlu Emekli İhtiyaç Kredisi"] 
+
+    for kredi in krediler:      #alias 
+     print(kredi)
+
+kredileriListele()
